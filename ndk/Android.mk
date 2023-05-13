610289e182e1d19776cb6333866f7ad41ee41db5
iamlooper <iamloooper@gmail.com>
2023-05-13 13:37:32 +0500

    feat: ndk changes - enable exceptions x2 (can be usable in future) - remove `-fno-rtti` flag because RTTI is disabled by default in `ndk-build`

--- ndk/Android.mk
+++ ndk/Android.mk
@@ -3,6 +3,7 @@ LOCAL_PATH := $(call my-dir)/src
 include $(CLEAR_VARS)
 LOCAL_MODULE := nrao
 LOCAL_SRC_FILES := nrao.cpp
+LOCAL_CPP_FEATURES += exceptions 
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
